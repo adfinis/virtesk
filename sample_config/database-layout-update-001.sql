a1786fa5330fc22c86c378b69b3f046971f5ce87
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2016-10-28 09:57:22 +0200

    Adjusted formatting to sql guide

--- sample_config/database-layout-update-001.sql
+++ sample_config/database-layout-update-001.sql
@@ -6,73 +6,93 @@ ALTER TABLE timed_thinclient_to_vm_mapping ADD resolution character varying NULL
 
 DROP VIEW current_thinclient_to_vm_mapping;
 CREATE VIEW current_thinclient_to_vm_mapping AS
-SELECT f.id,
-       f.thinclient,
-       f.vm,
-       f.resolution,
-       f.start_date,
-       f.end_date,
-       f.prio,
-       f.shutdown_vm
-FROM
-  (
-     (SELECT timed_thinclient_to_vm_mapping.id,
-             timed_thinclient_to_vm_mapping.thinclient,
-             timed_thinclient_to_vm_mapping.vm,
-             timed_thinclient_to_vm_mapping.resolution,
-             timed_thinclient_to_vm_mapping.start_date,
-             timed_thinclient_to_vm_mapping.end_date,
-             0 AS prio,
-             timed_thinclient_to_vm_mapping.shutdown_vm
-      FROM timed_thinclient_to_vm_mapping
-      WHERE ((timed_thinclient_to_vm_mapping.start_date IS NULL)
-             AND (timed_thinclient_to_vm_mapping.end_date IS NULL))
-      UNION SELECT timed_thinclient_to_vm_mapping.id,
-                   timed_thinclient_to_vm_mapping.thinclient,
-                   timed_thinclient_to_vm_mapping.vm,
-                   timed_thinclient_to_vm_mapping.resolution,
-                   timed_thinclient_to_vm_mapping.start_date,
-                   timed_thinclient_to_vm_mapping.end_date,
-                   1 AS prio,
-                   timed_thinclient_to_vm_mapping.shutdown_vm
-      FROM timed_thinclient_to_vm_mapping
-      WHERE ((timed_thinclient_to_vm_mapping.start_date <= now())
-             AND (timed_thinclient_to_vm_mapping.end_date IS NULL)))
-   UNION SELECT timed_thinclient_to_vm_mapping.id,
-                timed_thinclient_to_vm_mapping.thinclient,
-                timed_thinclient_to_vm_mapping.vm,
-                timed_thinclient_to_vm_mapping.resolution,
-                timed_thinclient_to_vm_mapping.start_date,
-                timed_thinclient_to_vm_mapping.end_date,
-                2 AS prio,
-                timed_thinclient_to_vm_mapping.shutdown_vm
-   FROM timed_thinclient_to_vm_mapping
-   WHERE ((now() >= timed_thinclient_to_vm_mapping.start_date)
-          AND (now() <= timed_thinclient_to_vm_mapping.end_date))) f
-ORDER BY f.prio DESC,
-         f.start_date,
-         f.id;
+SELECT
+    f.id,
+    f.thinclient,
+    f.vm,
+    f.resolution,
+    f.start_date,
+    f.end_date,
+    f.prio,
+    f.shutdown_vm
+FROM (
+    SELECT
+        timed_thinclient_to_vm_mapping.id,
+        timed_thinclient_to_vm_mapping.thinclient,
+        timed_thinclient_to_vm_mapping.vm,
+        timed_thinclient_to_vm_mapping.resolution,
+        timed_thinclient_to_vm_mapping.start_date,
+        timed_thinclient_to_vm_mapping.end_date,
+        0 AS prio,
+        timed_thinclient_to_vm_mapping.shutdown_vm
+    FROM
+        timed_thinclient_to_vm_mapping
+    WHERE (
+        (timed_thinclient_to_vm_mapping.start_date IS NULL)
+        AND
+        (timed_thinclient_to_vm_mapping.end_date IS NULL)
+    )
+    UNION SELECT
+        timed_thinclient_to_vm_mapping.id,
+        timed_thinclient_to_vm_mapping.thinclient,
+        timed_thinclient_to_vm_mapping.vm,
+        timed_thinclient_to_vm_mapping.resolution,
+        timed_thinclient_to_vm_mapping.start_date,
+        timed_thinclient_to_vm_mapping.end_date,
+        1 AS prio,
+        timed_thinclient_to_vm_mapping.shutdown_vm
+    FROM
+        timed_thinclient_to_vm_mapping
+    WHERE (
+        (timed_thinclient_to_vm_mapping.start_date <= now())
+        AND
+        (timed_thinclient_to_vm_mapping.end_date IS NULL))
+    UNION SELECT
+        timed_thinclient_to_vm_mapping.id,
+        timed_thinclient_to_vm_mapping.thinclient,
+        timed_thinclient_to_vm_mapping.vm,
+        timed_thinclient_to_vm_mapping.resolution,
+        timed_thinclient_to_vm_mapping.start_date,
+        timed_thinclient_to_vm_mapping.end_date,
+        2 AS prio,
+        timed_thinclient_to_vm_mapping.shutdown_vm
+    FROM
+        timed_thinclient_to_vm_mapping
+    WHERE (
+        (now() >= timed_thinclient_to_vm_mapping.start_date)
+        AND
+        (now() <= timed_thinclient_to_vm_mapping.end_date)
+    )
+) f
+ORDER BY
+    f.prio DESC,
+    f.start_date,
+    f.id;
 
 
 ALTER TABLE public.current_thinclient_to_vm_mapping OWNER TO "vdi-dbadmin";
 
 DROP VIEW thinclient_everything_view;
-CREATE VIEW thinclient_everything_view AS
-SELECT DISTINCT s.thinclient,
-                c.vm,
-                c.resolution,
-                s.dhcp_hostname,
-                s.systemuuid,
-                c.id,
-                c.start_date,
-                c.end_date,
-                c.prio,
-                c.shutdown_vm
-FROM (sysinfo_to_thinclient_mapping s
-      LEFT JOIN current_thinclient_to_vm_mapping c ON (((s.thinclient)::text = (c.thinclient)::text)))
-ORDER BY c.prio DESC,
-         c.start_date,
-         c.id;
+SELECT DISTINCT
+    s.thinclient,
+    c.vm,
+    c.resolution,
+    s.dhcp_hostname,
+    s.systemuuid,
+    c.id,
+    c.start_date,
+    c.end_date,
+    c.prio,
+    c.shutdown_vm
+FROM
+    sysinfo_to_thinclient_mapping s
+    LEFT JOIN (
+        current_thinclient_to_vm_mapping c ON (((s.thinclient)::text = (c.thinclient)::text))
+    )
+ORDER BY
+    c.prio DESC,
+    c.start_date,
+    c.id;
 
 
 ALTER TABLE public.thinclient_everything_view OWNER TO "vdi-dbadmin";
--- sample_config/database-layout.sql
+++ sample_config/database-layout.sql
@@ -25,7 +25,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 --
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
- COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 SET search_path = PUBLIC,
@@ -42,13 +42,14 @@ SET default_with_oids = FALSE;
 --
 
 CREATE TABLE timed_thinclient_to_vm_mapping (
-        vm character varying NOT NULL,
-        thinclient character varying NOT NULL,
-        resolution character varying NULL,
-        start_date TIMESTAMP WITH TIME ZONE,
-        end_date TIMESTAMP WITH TIME ZONE,
-        id bigint NOT NULL,
-        shutdown_vm boolean DEFAULT FALSE NOT NULL);
+    vm character varying NOT NULL,
+    thinclient character varying NOT NULL,
+    resolution character varying NULL,
+    start_date TIMESTAMP WITH TIME ZONE,
+    end_date TIMESTAMP WITH TIME ZONE,
+    id bigint NOT NULL,
+    shutdown_vm boolean DEFAULT FALSE NOT NULL
+);
 
 
 ALTER TABLE public.timed_thinclient_to_vm_mapping OWNER TO "vdi-dbadmin";
@@ -58,52 +59,68 @@ ALTER TABLE public.timed_thinclient_to_vm_mapping OWNER TO "vdi-dbadmin";
 --
 
 CREATE VIEW current_thinclient_to_vm_mapping AS
-SELECT f.id,
-       f.thinclient,
-       f.vm,
-       f.resolution,
-       f.start_date,
-       f.end_date,
-       f.prio,
-       f.shutdown_vm
-FROM
-  (
-     (SELECT timed_thinclient_to_vm_mapping.id,
-             timed_thinclient_to_vm_mapping.thinclient,
-             timed_thinclient_to_vm_mapping.vm,
-             timed_thinclient_to_vm_mapping.resolution,
-             timed_thinclient_to_vm_mapping.start_date,
-             timed_thinclient_to_vm_mapping.end_date,
-             0 AS prio,
-             timed_thinclient_to_vm_mapping.shutdown_vm
-      FROM timed_thinclient_to_vm_mapping
-      WHERE ((timed_thinclient_to_vm_mapping.start_date IS NULL)
-             AND (timed_thinclient_to_vm_mapping.end_date IS NULL))
-      UNION SELECT timed_thinclient_to_vm_mapping.id,
-                   timed_thinclient_to_vm_mapping.thinclient,
-                   timed_thinclient_to_vm_mapping.vm,
-                   timed_thinclient_to_vm_mapping.resolution,
-                   timed_thinclient_to_vm_mapping.start_date,
-                   timed_thinclient_to_vm_mapping.end_date,
-                   1 AS prio,
-                   timed_thinclient_to_vm_mapping.shutdown_vm
-      FROM timed_thinclient_to_vm_mapping
-      WHERE ((timed_thinclient_to_vm_mapping.start_date <= now())
-             AND (timed_thinclient_to_vm_mapping.end_date IS NULL)))
-   UNION SELECT timed_thinclient_to_vm_mapping.id,
-                timed_thinclient_to_vm_mapping.thinclient,
-                timed_thinclient_to_vm_mapping.vm,
-                timed_thinclient_to_vm_mapping.resolution,
-                timed_thinclient_to_vm_mapping.start_date,
-                timed_thinclient_to_vm_mapping.end_date,
-                2 AS prio,
-                timed_thinclient_to_vm_mapping.shutdown_vm
-   FROM timed_thinclient_to_vm_mapping
-   WHERE ((now() >= timed_thinclient_to_vm_mapping.start_date)
-          AND (now() <= timed_thinclient_to_vm_mapping.end_date))) f
-ORDER BY f.prio DESC,
-         f.start_date,
-         f.id;
+SELECT
+    f.id,
+    f.thinclient,
+    f.vm,
+    f.resolution,
+    f.start_date,
+    f.end_date,
+    f.prio,
+    f.shutdown_vm
+FROM (
+    SELECT
+        timed_thinclient_to_vm_mapping.id,
+        timed_thinclient_to_vm_mapping.thinclient,
+        timed_thinclient_to_vm_mapping.vm,
+        timed_thinclient_to_vm_mapping.resolution,
+        timed_thinclient_to_vm_mapping.start_date,
+        timed_thinclient_to_vm_mapping.end_date,
+        0 AS prio,
+        timed_thinclient_to_vm_mapping.shutdown_vm
+    FROM
+        timed_thinclient_to_vm_mapping
+    WHERE (
+        (timed_thinclient_to_vm_mapping.start_date IS NULL)
+        AND
+        (timed_thinclient_to_vm_mapping.end_date IS NULL)
+    )
+    UNION SELECT
+        timed_thinclient_to_vm_mapping.id,
+        timed_thinclient_to_vm_mapping.thinclient,
+        timed_thinclient_to_vm_mapping.vm,
+        timed_thinclient_to_vm_mapping.resolution,
+        timed_thinclient_to_vm_mapping.start_date,
+        timed_thinclient_to_vm_mapping.end_date,
+        1 AS prio,
+        timed_thinclient_to_vm_mapping.shutdown_vm
+    FROM
+        timed_thinclient_to_vm_mapping
+    WHERE (
+        (timed_thinclient_to_vm_mapping.start_date <= now())
+        AND
+        (timed_thinclient_to_vm_mapping.end_date IS NULL))
+    UNION SELECT
+        timed_thinclient_to_vm_mapping.id,
+        timed_thinclient_to_vm_mapping.thinclient,
+        timed_thinclient_to_vm_mapping.vm,
+        timed_thinclient_to_vm_mapping.resolution,
+        timed_thinclient_to_vm_mapping.start_date,
+        timed_thinclient_to_vm_mapping.end_date,
+        2 AS prio,
+        timed_thinclient_to_vm_mapping.shutdown_vm
+    FROM
+        timed_thinclient_to_vm_mapping
+    WHERE (
+        (now() >= timed_thinclient_to_vm_mapping.start_date)
+        AND
+        (now() <= timed_thinclient_to_vm_mapping.end_date)
+    )
+) f
+ORDER BY
+    f.prio DESC,
+    f.start_date,
+    f.id;
 
 
 ALTER TABLE public.current_thinclient_to_vm_mapping OWNER TO "vdi-dbadmin";
@@ -113,13 +130,16 @@ ALTER TABLE public.current_thinclient_to_vm_mapping OWNER TO "vdi-dbadmin";
 --
 
 CREATE VIEW dhcphostname_to_thinclient_auto_mapping AS
-SELECT DISTINCT (((timed_thinclient_to_vm_mapping.thinclient)::text || '.thinclients.yourdomain.site'::text))::character varying AS dhcp_hostname,
-                timed_thinclient_to_vm_mapping.thinclient
-FROM timed_thinclient_to_vm_mapping
-UNION
-SELECT DISTINCT timed_thinclient_to_vm_mapping.thinclient AS dhcp_hostname,
-                timed_thinclient_to_vm_mapping.thinclient
-FROM timed_thinclient_to_vm_mapping;
+SELECT DISTINCT
+    (((timed_thinclient_to_vm_mapping.thinclient)::text || '.thinclients.yourdomain.site'::text))::character varying AS dhcp_hostname,
+    timed_thinclient_to_vm_mapping.thinclient
+FROM
+    timed_thinclient_to_vm_mapping
+UNION SELECT DISTINCT
+    timed_thinclient_to_vm_mapping.thinclient AS dhcp_hostname,
+    timed_thinclient_to_vm_mapping.thinclient
+FROM
+    timed_thinclient_to_vm_mapping;
 
 
 ALTER TABLE public.dhcphostname_to_thinclient_auto_mapping OWNER TO "vdi-dbadmin";
@@ -129,7 +149,9 @@ ALTER TABLE public.dhcphostname_to_thinclient_auto_mapping OWNER TO "vdi-dbadmin
 --
 
 CREATE TABLE dhcphostname_to_thinclient_mapping (
-	dhcp_hostname character varying NOT NULL,                                                                          thinclient character varying NOT NULL);
+    dhcp_hostname character varying NOT NULL,
+    thinclient character varying NOT NULL
+);
 
 
 ALTER TABLE public.dhcphostname_to_thinclient_mapping OWNER TO "vdi-dbadmin";
@@ -139,8 +161,9 @@ ALTER TABLE public.dhcphostname_to_thinclient_mapping OWNER TO "vdi-dbadmin";
 --
 
 CREATE TABLE systemuuid_to_thinclient_mapping (
-       	systemuuid character varying NOT NULL,
-        thinclient character varying NOT NULL);
+    systemuuid character varying NOT NULL,
+    thinclient character varying NOT NULL
+);
 
 
 ALTER TABLE public.systemuuid_to_thinclient_mapping OWNER TO "vdi-dbadmin";
@@ -150,28 +173,36 @@ ALTER TABLE public.systemuuid_to_thinclient_mapping OWNER TO "vdi-dbadmin";
 --
 
 CREATE VIEW sysinfo_to_thinclient_mapping AS
-SELECT f.dhcp_hostname,
-       f.systemuuid,
-       f.thinclient,
-       f.prio
-FROM
-  (
-     (SELECT dhcphostname_to_thinclient_auto_mapping.dhcp_hostname,
-             NULL::character varying AS systemuuid,
-             dhcphostname_to_thinclient_auto_mapping.thinclient,
-             100 AS prio
-      FROM dhcphostname_to_thinclient_auto_mapping
-      UNION SELECT dhcphostname_to_thinclient_mapping.dhcp_hostname,
-                   NULL::character varying AS systemuuid,
-                   dhcphostname_to_thinclient_mapping.thinclient,
-                   200 AS prio
-      FROM dhcphostname_to_thinclient_mapping)
-   UNION SELECT NULL::character varying AS dhcp_hostname,
-                systemuuid_to_thinclient_mapping.systemuuid,
-                systemuuid_to_thinclient_mapping.thinclient,
-                300 AS prio
-   FROM systemuuid_to_thinclient_mapping) f
-ORDER BY f.prio DESC;
+SELECT
+    f.dhcp_hostname,
+    f.systemuuid,
+    f.thinclient,
+    f.prio
+FROM (
+    SELECT
+        dhcphostname_to_thinclient_auto_mapping.dhcp_hostname,
+        NULL::character varying AS systemuuid,
+        dhcphostname_to_thinclient_auto_mapping.thinclient,
+        100 AS prio
+    FROM
+        dhcphostname_to_thinclient_auto_mapping
+    UNION SELECT
+        dhcphostname_to_thinclient_mapping.dhcp_hostname,
+        NULL::character varying AS systemuuid,
+        dhcphostname_to_thinclient_mapping.thinclient,
+        200 AS prio
+    FROM
+        dhcphostname_to_thinclient_mapping
+    UNION SELECT
+        NULL::character varying AS dhcp_hostname,
+        systemuuid_to_thinclient_mapping.systemuuid,
+        systemuuid_to_thinclient_mapping.thinclient,
+        300 AS prio
+    FROM
+        systemuuid_to_thinclient_mapping
+) f
+ORDER BY
+    f.prio DESC;
 
 
 ALTER TABLE public.sysinfo_to_thinclient_mapping OWNER TO "vdi-dbadmin";
@@ -181,21 +212,26 @@ ALTER TABLE public.sysinfo_to_thinclient_mapping OWNER TO "vdi-dbadmin";
 --
 
 CREATE VIEW thinclient_everything_view AS
-SELECT DISTINCT s.thinclient,
-                c.vm,
-                c.resolution,
-                s.dhcp_hostname,
-                s.systemuuid,
-                c.id,
-                c.start_date,
-                c.end_date,
-                c.prio,
-                c.shutdown_vm
-FROM (sysinfo_to_thinclient_mapping s
-      LEFT JOIN current_thinclient_to_vm_mapping c ON (((s.thinclient)::text = (c.thinclient)::text)))
-ORDER BY c.prio DESC,
-         c.start_date,
-         c.id;
+SELECT DISTINCT
+    s.thinclient,
+    c.vm,
+    c.resolution,
+    s.dhcp_hostname,
+    s.systemuuid,
+    c.id,
+    c.start_date,
+    c.end_date,
+    c.prio,
+    c.shutdown_vm
+FROM
+    sysinfo_to_thinclient_mapping s
+    LEFT JOIN (
+        current_thinclient_to_vm_mapping c ON (((s.thinclient)::text = (c.thinclient)::text))
+    )
+ORDER BY
+    c.prio DESC,
+    c.start_date,
+    c.id;
 
 
 ALTER TABLE public.thinclient_everything_view OWNER TO "vdi-dbadmin";
@@ -245,7 +281,7 @@ ALTER TABLE ONLY timed_thinclient_to_vm_mapping ADD CONSTRAINT timed_thinclient_
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
- REVOKE ALL ON SCHEMA PUBLIC
+REVOKE ALL ON SCHEMA PUBLIC
 FROM PUBLIC;
 
 REVOKE ALL ON SCHEMA PUBLIC
