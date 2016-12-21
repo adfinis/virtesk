21fde8e78f3cbe56df5726f3cd70d52b802a40e7
Andrea Bettich <andrea.bettich@adfinis-sygroup.ch>
2016-12-21 15:18:40 +0100

    Additional tests for hostnamectl

--- virtesk-tc-connectspice/test_find_thinclient_identifier_nmcli.py
+++ virtesk-tc-connectspice/test_find_thinclient_identifier_nmcli.py
@@ -1,7 +1,7 @@
 import find_thinclient_identifier_nmcli as find_nmcli
 
 
-def test_extract_identifiers_from_nmcli(mock, nmcli_device_show, nmcli_con_show, nmcli_con_show_empty):
+def test_extract_identifiers_from_nmcli(mock, nmcli_device_show, nmcli_con_show, nmcli_con_show_empty, hostnamectl_transient):
     def my_check_output(cmd, **kwargs):
         if cmd[:4] == ['nmcli', 'con', 'show', 'ADSY-EAP']:
             return nmcli_con_show
@@ -9,10 +9,12 @@ def test_extract_identifiers_from_nmcli(mock, nmcli_device_show, nmcli_con_show,
             return nmcli_con_show_empty
         elif cmd[:3] == ['nmcli', 'device', 'show']:
             return nmcli_device_show
+        elif cmd[:1] == ['hostnamectl']:
+            return hostnamectl_transient
     mock.patch('find_thinclient_identifier_nmcli.subprocess.check_output', my_check_output)
     return_value = find_nmcli.extract_identifiers_from_nmcli()
 
-    assert return_value == (['a-hostname-appeared'], ['172.17.0.1', '10.9.5.185', '127.0.0.1'])
+    assert return_value == (['a-hostname-appeared', 'transient-hostname'], ['172.17.0.1', '10.9.5.185', '127.0.0.1'])
 
 
 def test_get_dhcp_hostname_from_connection(mock, nmcli_con_show):
@@ -46,3 +48,17 @@ def test_get_active_connections(mock, active_connection):
     patched_check_output.return_value = active_connection
     return_value = find_nmcli.get_active_connections()
     assert return_value == 2
+
+
+def test_get_hostname_from_hostnamectl(mock, hostnamectl_transient):
+    patched_check_output = mock.patch('find_thinclient_identifier_nmcli.subprocess.check_output')
+    patched_check_output.return_value = hostnamectl_transient
+    return_value = find_nmcli.extract_hostname_from_hostnamectl()
+    assert return_value == 'transient-hostname'
+
+
+def test_get_hostname_from_hostnamectl_empty(mock, hostnamectl_transient_empty):
+    patched_check_output = mock.patch('find_thinclient_identifier_nmcli.subprocess.check_output')
+    patched_check_output.return_value = hostnamectl_transient_empty
+    return_value = find_nmcli.extract_hostname_from_hostnamectl()
+    assert return_value == None
